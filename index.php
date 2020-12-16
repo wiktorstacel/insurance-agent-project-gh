dc5b1e0e648f1292b9f86aaa81a733b427d48224
wiktorstacel <stawik964@gmail.com>
2020-12-16 18:53:55 +0100

    Articles edition modification

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+require ('strona_stage.inc');
+
+$stronaglowna = new Strona2();
+
+$stronaglowna -> title = 'Ubezpieczenia komunikacyjne Rzeszów';
+
+$stronaglowna -> keywords = 'ubezpieczenia, komunikacyjne, rzeszów, podkarpackie';
+
+$stronaglowna -> description = '...Zwrócić uwagę na wylicznenie składki ubezpiecznia kominukacyjnego...';
+
+$stronaglowna -> Wyswietl();
+
+?>
