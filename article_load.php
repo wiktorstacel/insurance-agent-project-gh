a08a0f1f82c035a9b85335a93db341e85a1ac18b
wiktorstacel <stawik964@gmail.com>
2021-01-06 15:31:10 +0100

    Add Article title to head-meta: title, keywords, desription

--- article_load.php
+++ article_load.php
@@ -4,6 +4,42 @@ require ('strona_stage.inc');
 
 class article_load extends Strona2
 {
+    //Set class variables also keywords and description for each article instead of doing it down below
+    function setTitle()
+    {
+        require 'config_db.php';
+        $article_id = htmlentities($_GET['article_id'], ENT_QUOTES, "UTF-8");
+        $result = mysqli_query($conn,
+                    sprintf("SELECT a.title, a.content, u.surname FROM articles a, users u WHERE a.user_id = u.user_id AND a.article_id = '%d'",
+                    mysqli_real_escape_string($conn, $article_id)
+                        ));                
+        if($result != TRUE){echo 'Bład zapytania MySQL, odpowiedź serwera: '.mysqli_error($conn);}
+        $row = mysqli_fetch_array($result, MYSQLI_NUM);
+        $this->title = $row[0];
+        
+        $pieces = explode(" ", $row[0]);
+        $length = count($pieces);
+        for ($i = 0; $i < $length; $i++) {
+            if(strlen($pieces[$i]) < 4)
+            {
+                unset($pieces[$i]);//delete index, there are other function that reindex array - for implode() works
+            }
+        }
+        $words_comma = implode(", ", $pieces);
+        $this->keywords = $words_comma;
+        
+        $this->description = $row[0];
+        return true; 
+    }
+    
+    public function WyswietlTytul()
+    {
+        echo '<meta name="verify-v1" content="TNJHeoK61VQS0o5RHL6ATlM5xAQS1jdwD34gtOl0LpI=" />
+                     <meta name="y_key" content="d53f09aab117dc53">
+                     <meta http-equiv="Content-Language" content="pl">';
+        $this->setTitle();
+        echo "<title> $this->title </title>\n";
+    }
     
     public function WyswietlPage()
     {
@@ -26,9 +62,13 @@ class article_load extends Strona2
                 echo $row[2];
                 echo '<br><br><b>Autor:</b> '.$row[4].', '.$row[3];
                 echo '<br /><br />';
-                mysqli_close($conn);
-                //$this -> title = $row[1]; jak ustawić z tego miejsca title na poziomie klasy
-                //include 'contact_formLoad.php?user_id=1';
+                
+                
+                
+                mysqli_close($conn);               
+ 
+                //$this -> title = $row[1]; jak ustawić z tego miejsca title na poziomie klasy. ODP:$this -> setSomething($s);
+
                 
                         echo'	<br><br><br><p><strong>Ubezpieczenia i Odszkodowania</strong> jest serwisem internetowym zarejstrowanym w <a href="http://www.freecsstemplates.org/">Free CSS Templates</a> released under a <a href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution 2.5 License</a>. You"re free to use it for both commercial or personal use. I only ask that you link back to <a href="http://www.freecsstemplates.org/">my site</a> in some way. <em>Enjoy :)</em></p>
 			<h2>Praesent Scelerisque</h2>
@@ -66,11 +106,11 @@ class article_load extends Strona2
 
 $article_load = new article_load();
 
-$article_load -> title = 'Artykuł';
+//$article_load -> title = 'Artykuł';
 
-$article_load -> keywords = 'ubezpieczenia, komunikacyjne, odszkodowania, rzeszów, podkarpackie';
+//$article_load -> keywords = 'ubezpieczenia, komunikacyjne, odszkodowania, rzeszów, podkarpackie';
 
-$article_load -> description = 'ubezpieczenia, odszkodowania';
+//$article_load -> description = 'ubezpieczenia, odszkodowania';
 
 $article_load -> Wyswietl();
 
