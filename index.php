da7c7e6b46573773613b2a76ae987cca08b85c21
wiktorstacel <stawik964@gmail.com>
2021-01-06 21:50:25 +0100

    Configuration of down page section. Clear Articles from HTML to divide string to show only 500 charakters on main page

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ require ('strona_stage.inc');
 
 $index = new Strona2();
 
-$index -> title = 'Ubezpieczenia komunikacyjne Rzeszów';
+$index -> title = 'Ubezpieczenia i Odszkodowania - Rzeszów';
 
-$index -> keywords = 'ubezpieczenia, komunikacyjne, rzeszów, podkarpackie';
+$index -> keywords = 'ubezpieczenia, odszkodowania, komunikacyjne, wypadkowe, rzeszów, podkarpackie';
 
-$index -> description = '...Zwrócić uwagę na wylicznenie składki ubezpiecznia kominukacyjnego...';
+$index -> description = 'Artykuły od doradców, specjalistów od ubezpieczeń i odszkodowań.';
 
 $index -> Wyswietl();
 
